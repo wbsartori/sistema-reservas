f2b4b3f34e55fbe95026baea70fa2a2d6d4accef
wbsartori <wbsartori@gmail.com>
2021-02-20 18:34:39 -0300

    Projeto em Django

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1 align="center">
+    <a href="https://github.com/wbsartori/reservas">🔗 Sistema de Reservas</a>
+</h1>
+<p align="center"> Sistema para reservas de salas, equipamentos e veículos. O sistema é open source.</p>
+
+<p align="center">
+ <a href="#objetivo">Objetivo</a> •
+ <a href="#roadmap">Roadmap</a> • 
+ <a href="#tecnologias">Tecnologias</a> • 
+ <a href="#contribuicao">Contribuição</a> • 
+ <a href="#licenc-a">Licença</a> • 
+ <a href="#autor">Autor</a>
+</p>
+
+### Obejtivo
+<p >
+    Disponibilizar um sistema de reservas de salas, equipamentos e veíclos mais completo possível e de código aberto.
+</p>    
+
+### Features
+
+- [ ] Cadastro de empresas
+- [ ] Cadastro de filiais
+- [X] Cadastro de equipamentos
+    - [X] Cadastro de marcas de equipamentos
+    - [X] Cadastro de tipos de equipamentos
+- [ ] Cadastro de horários
+- [X] Cadastro de usuários
+- [ ] Cadastro de salas
+- [ ] Cadastro de veículos
+    - [X] Cadastro de cores de veículos
+    - [X] Cadastro de marcas de veículos
+    - [X] Cadastro de modelos de veículos
+    - [X] Cadastro de tipo de combústivel
+    - [X] Cadastro de tipo de veículos
+- [X] Painel Administrativo
+- [X] Permissões de Usuários
+- [ ] Reservas
+  - [ ] Reservar Equipamento
+  - [ ] Reservar Sala
+  - [ ] Reservar Veículo
+  - [ ] Reservar mais de um item
+
+<h4 align="center"> 
+	🚧  Sistema de Reservas 🚀 Em construção...  🚧
+</h4>
---
+++ app/backend/admin.py
@@ -0,0 +1,97 @@
+from django.contrib import admin
+from .models import *
+
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('descricao','uf')
+
+    def get_queryset(self, request):
+        queryset = super(EstadoAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+class cidadeAdmin(admin.ModelAdmin):
+    list_display = ('codigo_ibge','cidade')
+
+    def get_queryset(self, request):
+        queryset = super(cidadeAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('cidade')
+        return queryset
+
+
+class departamentoAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(departamentoAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+class EquipamentoMarcaAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(EquipamentoMarcaAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+class EquipamentoTipoAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(EquipamentoTipoAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+class VeiculoCorAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(VeiculoCorAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+class VeiculoMarcaAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(VeiculoMarcaAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+
+class VeiculoModeloAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(VeiculoModeloAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+class VeiculoTipoCombustivelAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(VeiculoTipoCombustivelAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+class VeiculoTipoVeiculoAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(VeiculoTipoVeiculoAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('descricao')
+        return queryset
+
+admin.site.register(Cidade, cidadeAdmin)
+admin.site.register(Estado, EstadoAdmin)
+admin.site.register(Departamentos, departamentoAdmin)
+admin.site.register(EquipamentoMarca, EquipamentoMarcaAdmin)
+admin.site.register(EquipamentoTipo, EquipamentoTipoAdmin)
+admin.site.register(VeiculoCor, VeiculoCorAdmin)
+admin.site.register(VeiculoMarca, VeiculoMarcaAdmin)
+admin.site.register(VeiculoModelo, VeiculoModeloAdmin)
+admin.site.register(VeiculoTipoCombustivel,VeiculoTipoCombustivelAdmin)
+admin.site.register(VeiculoTipoVeiculo,VeiculoTipoVeiculoAdmin)
+
+admin.site.register(EmpDepartamento)
+admin.site.register(EmpEmpresa)
+admin.site.register(EmpFilial)
+admin.site.register(Equipamento)
+admin.site.register(Sala)
+admin.site.register(Veiculo)
+
+
+
