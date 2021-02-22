4668c7f5c77e71536ea0231fb75ad44fece74be4
wbsartori <wbsartori@gmail.com>
2021-02-22 12:53:10 -0300

    Ajuste nos templates básicos e criação das rotas.

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -6,13 +6,13 @@ def index(request):
     return render(request, 'home/index.html')
 
 def equipamento(request):
-    return render(request, 'reservas/equipamentos.html')
+    return render(request, 'equipamentos/equipamentos.html')
 
 def sala(request):
-    return render(request, 'reservas/salas.html')
+    return render(request, 'salas/salas.html')
 
 def veiculo(request):
-    return render(request, 'reservas/veiculos.html')
+    return render(request, 'veiculos/veiculos.html')
 
 def multiplo(request):
     return render(request, 'reservas/multiplos.html')
