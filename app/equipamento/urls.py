22ab02014e928ebefc9ea02dd654b0a4f9648ee2
wbsartori <wbsartori@gmail.com>
2021-03-01 22:57:13 -0300

    Ajustado cadastro de equipamentos.

--- app/equipamento/urls.py
+++ app/equipamento/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/', equipamento, name='equipamento'),
+    path('', equipamento, name='equipamento'),
     path('equipamento/novo/', novo_equip, name='novo_equip'),
 ]
--- app/equipamento/views.py
+++ app/equipamento/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
+from backend.models import AuthUser
+from reserva.models import Reserva
 
 from .forms import ReservarEquipamentoForm
 
 def equipamento(request):
-    return render(request, 'equipamentos.html')
-
-def novo_equip(request):
-    form = ReservarEquipamentoForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+    reservas = Reserva.objects.all()
+    usuarios = AuthUser.objects.all()
 
     context = {
-        'form': form
+        'reservas': reservas,
+        'usuarios': usuarios
     }
+    return render(request, 'equipamentos.html', context)
+
+def novo_equip(request):
+    form = ReservarEquipamentoForm()
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = ReservarEquipamentoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request, 'equipamentos.html', context)
+
     return render(request, 'novo_equip.html', context)
