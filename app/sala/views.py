9dc95c11d851166e2ed5944110d9753f319e33fa
wbsartori <wbsartori@gmail.com>
2021-03-02 07:24:53 -0300

    Reserva de salas, equipamentos e Multiplos ajustado.

--- app/sala/views.py
+++ app/sala/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
+from backend.models import AuthUser
+from reserva.models import Reserva
 
 from .forms import ReservarSalaForm
 
-# Create your views here.
-def sala(request):
-    return render(request, 'salas.html')
 
-def novo_sala(request):
-    form = ReservarSalaForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+def sala(request):
+    reservas = Reserva.objects.all()
+    usuarios = AuthUser.objects.all()
 
     context = {
-        'form': form
+        'reservas': reservas,
+        'usuarios': usuarios
     }
+    return render(request, 'salas.html', context)
+
+
+def novo_sala(request):
+    form = ReservarSalaForm(request.POST)
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = ReservarSalaForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request, 'salas.html', context)
 
     return render(request, 'novo_sala.html', context)
