5435b41766588625ac04975b25fed7749225d0b6
wbsartori <wbsartori@gmail.com>
2021-03-08 23:23:45 -0300

    Delete reserva de veiculos ok.

--- app/veiculo/urls.py
+++ app/veiculo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import veiculo, novo_veiculo
+from .views import veiculo, novo_veiculo, deletar_veiculo
 
 urlpatterns = [
     path('', veiculo, name='veiculo'),
     path('veiculo/novo/', novo_veiculo, name='novo_veiculo'),
+    path('veiculo/deletar/<int:id>', deletar_veiculo, name='deletar_veiculo'),
 ]
--- app/veiculo/views.py
+++ app/veiculo/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from reserva.models import Reserva
 from backend.models import AuthUser
-from .models import VeiculoMarca, VeiculoModelo
+from .models import VeiculoMarca, VeiculoModelo, Veiculo
 from .forms import ReservarVeiculoForm
+from django.contrib import messages
 # Create your views here.
 
 def veiculo(request):
@@ -11,12 +12,14 @@ def veiculo(request):
     usuarios = AuthUser.objects.all()
     veiculosMarca = VeiculoMarca.objects.all()
     veiculosModelo = VeiculoModelo.objects.all()
+    veiculo = Veiculo.objects.all()
 
     context = {
         'reservas': reservas,
         'usuarios' : usuarios,
         'veiculosMarca': veiculosMarca,
-        'veiculoModelo': veiculosModelo
+        'veiculoModelo': veiculosModelo,
+        'veiculo': veiculo
     }
     return render(request, 'veiculos.html', context)
 
@@ -32,3 +35,10 @@ def novo_veiculo(request):
         return render(request, 'veiculos.html', context)
 
     return render(request, 'novo_veiculos.html', context)
+
+def deletar_veiculo(request, id):
+    reserva = get_object_or_404(Reserva, pk=id)
+    reserva.delete()
+    messages.info(request, 'Reserva de veiculo deletada com sucesso!')
+    return redirect('veiculo')
+
